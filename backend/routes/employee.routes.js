c6792c2f9785df1f4b423879b1f00d2e55db263a
mymom0937 <mymom0937@gmail.com>
2024-11-02 21:38:49 +0000

    second commit

--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -8,7 +8,12 @@ const employeeController = require('../controllers/employee.controller');
 // Import middleware 
 const authMiddleware = require("../middlewares/auth.middleware");
 // Create a route to handle the add employee request on post
-router.post("/api/employee", [authMiddleware.verifyToken, authMiddleware.isAdmin], employeeController.createEmployee);
+router.post("/api/employee",
+  
+  // [authMiddleware.verifyToken, authMiddleware.isAdmin], 
+  employeeController.createEmployee
+
+);
 // Create a route to handle the get all employees request on get
 router.get(
   "/api/employees",
